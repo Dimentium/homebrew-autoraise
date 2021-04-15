f22fb349cde9c5cbf2e181695cb37dce7632201f
Dmitry Kuznetsov <dk@DKMBA.local>
2021-04-15 11:59:55 +0200

    Initial commit

---
+++ autoraise.rb
@@ -0,0 +1,48 @@
+class Autoraise < Formula
+  desc "Focus-follows-mouse for MacOS"
+  homepage "https://github.com/sbmpost/AutoRaise"
+  # url "https://github.com/sbmpost/AutoRaise/archive/refs/heads/master.zip"
+  url "https://github.com/sbmpost/AutoRaise.git"
+  version "1.7d"
+  sha256 ""
+  license "GPL-3.0"
+
+  head do
+    url "https://github.com/sbmpost/AutoRaise.git"
+  end
+
+  def install
+    system "make clean"
+    system "make"
+    bin.install "AutoRaise"
+  end
+
+  plist_options :manual => "AutoRaise"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+          <string>#{opt_bin}/AutoRaise</string>
+          </array>
+          <key>StandardErrorPath</key>
+          <string>#{ENV["HOME"]}/Library/Logs/AutoRaise.log</string>
+          <key>StandardOutPath</key>
+          <string>#{ENV["HOME"]}/Library/Logs/AutoRaise.log</string>
+          <key>RunAtLoad</key>
+          <true/>
+        </dict>
+      </plist>
+    EOS
+  end
+
+  test do
+    system "false"
+  end
+end
