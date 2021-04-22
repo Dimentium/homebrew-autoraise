4b0d0d3da2923a447fd50f9d24ac06199676eef7
DK <swpbox@gmail.com>
2021-04-22 12:36:54 +0200

    Updated to 1.8

--- autoraise.rb
+++ autoraise.rb
@@ -3,7 +3,7 @@ class Autoraise < Formula
   homepage "https://github.com/sbmpost/AutoRaise"
   # url "https://github.com/sbmpost/AutoRaise/archive/refs/heads/master.zip"
   url "https://github.com/sbmpost/AutoRaise.git"
-  version "1.7e"
+  version "1.8"
   sha256 ""
   license "GPL-3.0"
 
